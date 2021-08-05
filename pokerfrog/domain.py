a0ae2bc430ddf2696352e4454d87827bf605f9b3
Alex Kosh <sasha23969@gmail.com>
2021-08-05 03:02:35 +0300

    feat: Better Card display

--- pokerfrog/domain.py
+++ pokerfrog/domain.py
@@ -2,12 +2,25 @@
 
 import dataclasses
 
-CARD_VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+CARD_VALUES = [
+    '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
+]
 CARD_SUITS = ['spades', 'diamonds', 'hearts', 'clubs']
 
 
 @dataclasses.dataclass
 class Card:
     """Class to repesent card object."""
+
     value: str
     suit: str
+
+    def __repr__(self):
+        suit = {
+            'spades': '♠',
+            'diamonds': '♦',
+            'hearts': '♥',
+            'clubs': '♣',
+        }[self.suit]
+
+        return f'[{self.value} {suit}]'
